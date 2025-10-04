2fc47d84cb2f3eeac8378e75239a4de763309745
EduarRugamas <juaneduardo021299@hotmail.com>
2025-10-04 09:38:18 -0600

    init commit

---
+++ api-prueba-tecnica/src/main/java/com/eduar/dev/prueba/ApiPruebaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.eduar.dev.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPruebaTecnicaApplication.class, args);
+	}
+
+}
---
+++ api-prueba-tecnica/src/test/java/com/eduar/dev/prueba/ApiPruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduar.dev.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
